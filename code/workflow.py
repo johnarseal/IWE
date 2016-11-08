5235558f289b540a4792b340cc4aa0c01b2347cc
aijohn122@live.cn <johnzz@pku.edu.cn>
2016-11-08 17:43:57 +0800

    directly store the display result in cache:

--- code/workflow.py
+++ code/workflow.py
@@ -10,6 +10,42 @@ UNIX_TIMESTAMP(ts3) - UNIX_TIMESTAMP(ts2),
 UNIX_TIMESTAMP(ts4)- UNIX_TIMESTAMP(ts3),
 UNIX_TIMESTAMP(ts5)- UNIX_TIMESTAMP(ts4) 
 """
+
+def createWfCacheTB(tbName,conn,cursor):
+    initCreateSql = "CREATE TABLE " + tbName +\
+    """(
+        transition VARCHAR(80) NOT NULL PRIMARY KEY,
+        num INT NOT NULL,
+        q1t0 DOUBLE(16,8),
+        q1t1 DOUBLE(16,8),
+        q1t2 DOUBLE(16,8),
+        q1t3 DOUBLE(16,8),
+        q1t4 DOUBLE(16,8),
+        q1t5 DOUBLE(16,8),
+        q2t0 DOUBLE(16,8),
+        q2t1 DOUBLE(16,8),
+        q2t2 DOUBLE(16,8),
+        q2t3 DOUBLE(16,8),
+        q2t4 DOUBLE(16,8),
+        q2t5 DOUBLE(16,8),
+        q3t0 DOUBLE(16,8),
+        q3t1 DOUBLE(16,8),
+        q3t2 DOUBLE(16,8),
+        q3t3 DOUBLE(16,8),
+        q3t4 DOUBLE(16,8),
+        q3t5 DOUBLE(16,8),
+        meant0 DOUBLE(16,8),
+        meant1 DOUBLE(16,8),
+        meant2 DOUBLE(16,8),
+        meant3 DOUBLE(16,8),
+        meant4 DOUBLE(16,8),
+        meant5 DOUBLE(16,8)
+    );
+    """
+    cursor.execute(initCreateSql)
+    conn.commit()
+
+
 def processWFdata(rawD):
     data = list(rawD)
     
@@ -62,7 +98,7 @@ def processWFdata(rawD):
 def fetchWorkflow(selectors):
     
     if len(selectors) == 0:
-        rawD = fetchInitWF()
+        return fetchInitWF()
     else:    
         sql = "SELECT transition, " + sqlTS + " FROM " + TD[session["DS"]]["statustran"]
         conSql = buildSQL(selectors)
@@ -71,10 +107,11 @@ def fetchWorkflow(selectors):
         cursor = conDB()
         cursor.execute(sql)
         rawD = cursor.fetchall()
-    if len(rawD) == 0:
-        return None
-    else:
-        return processWFdata(rawD)
+        if len(rawD) == 0:
+            return None
+        else:
+            return processWFdata(rawD)
+
 
 # this is to optimize the initial time spent on fetching workflow data
 def fetchInitWF():
@@ -84,15 +121,59 @@ def fetchInitWF():
     cursor = conn.cursor()
     
     # see whether the cache table exists
-    if cursor.execute(sql) == 0:
-        # if not exists, build the cache table
-        newTBSql = "CREATE TABLE " + tbName + " AS SELECT transition, " + sqlTS + " FROM " + TD[session["DS"]]["statustran"]
-        cursor.execute(newTBSql)
+    
+    # if not exists, build the cache table and store the cache
+    if cursor.execute(sql) == 0:    
+        fetchSQL = "SELECT transition, " + sqlTS + " FROM " + TD[session["DS"]]["statustran"]
+        cursor.execute(fetchSQL)
+        rawD = cursor.fetchall()
+        if len(rawD) == 0:
+            return None
+        else:
+            proData = processWFdata(rawD)        
+        
+        # create the cache table
+        maxLen = 6
+        createWfCacheTB(tbName,conn,cursor)
+        
+        # store the data in cache table
+        for tranStr in proData:
+            row = proData[tranStr]
+            insertSql = "INSERT INTO " + tbName + " VALUES('" + tranStr + "', " + str(row["num"])
+            items = ("q1","ts","q3","meanTS",)
+            for item in items:
+                for index in range(len(row[item])):                
+                    insertSql += ", " + str(row[item][index])
+                for i in range(maxLen - len(row[item])):
+                    insertSql += ", NULL"
+            insertSql += ")"
+            cursor.execute(insertSql)
         conn.commit()
 
     sql = "SELECT * FROM " + tbName
     cursor.execute(sql)
-    return cursor.fetchall()
+    rawD = cursor.fetchall()
+    rtData = {}
+    for row in rawD:
+        tranStr = row[0]
+        rtData[tranStr] = {}
+        rtData[tranStr]["num"] = row[1]
+        rtData[tranStr]["q1"] = []
+        rtData[tranStr]["q3"] = []
+        rtData[tranStr]["ts"] = []
+        rtData[tranStr]["meanTS"] = []
+        for i in range(2,8):
+            if(row[i]) == None:
+                break
+            rtData[tranStr]["q1"].append(row[i])
+        for i in range(8,14):
+            rtData[tranStr]["ts"].append(row[i])
+        for i in range(14,20):
+            rtData[tranStr]["q3"].append(row[i])
+        for i in range(20,26):
+            rtData[tranStr]["meanTS"].append(row[i])
+    
+    return rtData
     
     
     
